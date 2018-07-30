ba379526c9960ed64c68b3c2fdc16d97a01dcd73
linyuan <2551487871@qq.com>
2018-07-30 11:07:49 +0800

    抽象产品

---
+++ AbstractFactory/abstract_product.h
@@ -0,0 +1,21 @@
+#ifndef __ABSTRACT_PRODUCT_H
+#define __ABSTRACT_PRODUCT_H
+
+#include <string>
+
+//车接口
+class Car
+{
+public:
+	virtual std::string name();
+};
+
+//自行车借口
+class Bike
+{
+public:
+	virtual std::string name();
+
+};
+
+#endif
