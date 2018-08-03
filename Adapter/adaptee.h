300fdc9ed484ac2fdbb627c29c87ea4c042b8dc1
linyuan <2551487871@qq.com>
2018-08-03 09:40:48 +0800

    适配者

---
+++ Adapter/adaptee.h
@@ -0,0 +1,15 @@
+#ifndef __ADAPTEE_H
+#define __ADAPTEE_H
+
+#include <iostream>
+//我自己的充电器
+class MyCharger
+{
+public:
+	void do_charge()
+	{
+		std::cout << "charging..." << std::endl;
+	}
+};
+
+#endif 
